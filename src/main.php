a1c9961464334f8986391f2f1a5ecab65cda8b59
XEmAX32 <XEmAX32@gmail.com>
2015-08-06 13:10:12 +0200

    Update main.php

--- src/main.php
+++ src/main.php
@@ -1,3 +1,16 @@
 <?php
+//promemoria finire, inserire namespace, inserire ultime funzioni chiudere le graffe
 
-namespace:
+namespace: 
+  
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerSpawnEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+
+class TEST extends PluginBase{
+
+public function $event OnPlayerSpawn(PlayerSpawnEvent)
+$sender->SendMessage(TextFormat::GREEN "[+]") $this GetPlayer->GetName(). (TextFormat::GREEN. " ");
+
+public function $event OnPlayerQuit(PlayerQuitEvent)
+$sender->SendMessage(TextFormat::RED "[-]") $this GetPlayer->GetName(). (TextFormat::RED. " ");
